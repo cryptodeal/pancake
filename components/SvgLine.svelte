e0ac983961cad8abfd952466ee06d489c3a7fe40
cryptodeal <jimmydeal16@gmail.com>
2021-05-15 04:26:39 -0500

    add plotNull option to SvgLine

--- components/SvgLine.svelte
+++ components/SvgLine.svelte
@@ -7,10 +7,23 @@
 	export let data;
 	export let x = default_x;
 	export let y = default_y;
+  export let plotNull = false;
+  let d;
 
-	$: d = 'M' + data
-		.map((d, i) => `${$x_scale(x(d, i))},${$y_scale(y(d, i))}`)
-		.join('L');
+  $: if (plotNull) {
+    d = 'M' + data
+      .filter((d, i) => y(d, i) != null)
+      .map((d, i) => `${$x_scale(x(d, i))},${$y_scale(y(d, i))}`)
+      .join('L')
+  } else {
+    d = data
+      .map((d, i) => {
+        let motion = 'L'
+        if (y(d) == null) return
+        if (i == 0 || y(data[i - 1]) == null) motion = 'M'
+        return `${motion}${$x_scale(x(d, i))},${$y_scale(y(d, i))}`
+      })
+  }
 </script>
 
 <slot {d}></slot>
